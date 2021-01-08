2317895becb8e5912fc8652d758b91b7213f993f
Oğuzcan ÖZDEMİR <ce.oguzcan.ozdemir@gmail.com>
2021-01-08 23:35:30 +0300

    socket io redis adapter

--- src/socketApi.js
+++ src/socketApi.js
@@ -5,8 +5,20 @@ const socketApi = {
     io: io
 };
 
+/**
+ * Redis adapter
+ */
+
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({
+    host: process.env.REDIS_URI,
+    port: process.env.REDIS_PORT
+}));
+
 io.on('connection', socket => {
    console.log('a user logged in');
+
+   socket.broadcast.emit('hello');
 });
 
 module.exports = socketApi;
