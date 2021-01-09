57bd3e0cbdceb5f60dd1dc258d168d36a45b2f85
Oğuzcan ÖZDEMİR <ce.oguzcan.ozdemir@gmail.com>
2021-01-09 12:27:27 +0300

    angular tab controller

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -1,7 +1,13 @@
 app.controller('chatController', ['$scope', ($scope) => {
+    $scope.activeTab = 2;
+
+    $scope.changeTab = tab => {
+        $scope.activeTab = tab;
+    };
+
     const socket = io.connect("http://localhost:3000");
 
-    socket.on('hello', () => {
+    /*socket.on('hello', () => {
         console.log('hello');
-    })
+    });*/
 }]);
